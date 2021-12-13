9f8469832fe9ff36e274053052206143fe3fb374
Ivan Valui <Wanjawaluj111@gmail.com>
2021-12-14 01:37:56 +0300

    Add Lab9

---
+++ Lab9/src/test/java/test/TestFxPro.java
@@ -0,0 +1,76 @@
+package test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import page.LoginPage;
+import page.TradingPage;
+import java.util.concurrent.TimeUnit;
+
+public class TestFxPro {
+    private WebDriver driver;
+    private TradingPage fxProTradingPage;
+
+    public String timeBeforeChanges;
+    public String timeAfterChanges;
+
+    public Boolean ordersBeforeChanges;
+    public Boolean ordersAfterChanges;
+
+    private String email = "Wanjawaluj111@gmail.com";
+    private String password = "sP22j8pQ_h$hyGy";
+
+    @BeforeMethod
+    public void browserSetup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        driver = new ChromeDriver(options);
+        fxProTradingPage = new LoginPage(driver)
+                .openPage()
+                .enterEmail(email)
+                .enterPassword(password)
+                .signIn()
+                .selectDemoAccount();
+    }
+
+    @Test(description = "Change time")
+    public void changeTimeAndReturnChanges() {
+
+        fxProTradingPage.openTimeSettings()
+                .changeTimeZoneTo12()
+                .closeTimeSettings();
+
+        timeBeforeChanges = fxProTradingPage.getTime();
+
+        fxProTradingPage.openTimeSettings()
+                .changeTimeZoneTo11()
+                .closeTimeSettings();
+
+        timeAfterChanges = fxProTradingPage.getTime();
+
+        Assert.assertNotEquals(timeBeforeChanges, timeAfterChanges);
+    }
+
+    @Test(description = "Create order")
+    public void createOrderAndReturnChanges() {
+
+        fxProTradingPage.openCreateOrderMenu()
+                .createOrder();
+
+        ordersAfterChanges = fxProTradingPage.getOrders();
+
+        fxProTradingPage.deleteOrder();
+
+        Assert.assertTrue(ordersAfterChanges);
+    }
+
+    @AfterMethod
+    public void browserShutDown() {
+        driver.quit();
+        driver = null;
+    }
+}
